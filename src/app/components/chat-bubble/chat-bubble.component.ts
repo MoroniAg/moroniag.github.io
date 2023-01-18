4e731f9d535ce1e791909fda44be16efb8c2bc9c
Moroni Aguilera <moroniaguilera1993@gmail.com>
2023-01-17 20:00:26 -0500

    #17 - work in progress in experience page

--- src/app/components/chat-bubble/chat-bubble.component.ts
+++ src/app/components/chat-bubble/chat-bubble.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Tittle } from 'src/app/models/Tittle';
+import { Value } from 'src/app/models/Values';
 
 @Component({
   selector: 'app-chat-bubble',
@@ -9,10 +10,34 @@ import { Tittle } from 'src/app/models/Tittle';
 export class ChatBubbleComponent implements OnInit {
 
   @Input() tittle!: Tittle;
-  
-  constructor() { }
+  @Input() color!: string;
+  constructor() {
+
+    let bubble = document.getElementsByClassName("bubble");
+
+
+    console.log(bubble)
+    console.log('color: '+this.color)
+    for (let index = 0; index < bubble.length; index++) {
+      const element = bubble[index];
+      if (element instanceof HTMLElement) {
+        element.style.backgroundColor = this.color
+      }
+    }
+
+    // bubble.style.backgroundColor="";
+  }
 
   ngOnInit(): void {
   }
+
+
+  findValue(tittle: Tittle, valueName: string): Value {
+
+    let value = tittle.values.find(value => value.name == valueName)
+    return value == null ? new Value : value;
+
+  }
+
 
 }
