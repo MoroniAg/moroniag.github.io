5f2ca750f9efa7beee8f86f19b75ab7b99c432d2
moroniaguilera <moroniaguilera1993@gmail.com>
2026-03-24 02:36:46 +0000

    F #60 feat: enhance AboutMe component with dynamic format display and profile image; add translations for About Me section

--- portfolio-ui/src/components/AboutMe/AboutMe.jsx
+++ portfolio-ui/src/components/AboutMe/AboutMe.jsx
@@ -1,3 +1,33 @@
+import { useEffect, useState } from "react";
+import OutputFormats from "./OutputFormats";
+import profileImg from "../../assets/images/profile.png";
+
 export default function AboutMe() {
-  return <div>About Me</div>;
+  const formats = ["java", "python", "html"];
+  const [format, setFormat] = useState("java");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFormat((prev) => {
+        const idx = formats.indexOf(prev);
+        return formats[(idx + 1) % formats.length];
+      });
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-4 lg:h-[calc(100vh-7.5rem)] justify-center items-center ">
+      <div className="flex items-center justify-center w-full h-[50%]">
+        <img
+          src={profileImg}
+          alt="Profile"
+          className=" h-[50%] md:h-110 lg:h-full rounded-full object-cover"
+        />
+      </div>
+      <div className="flex-none">
+        <OutputFormats format={format} />
+      </div>
+    </div>
+  );
 }
