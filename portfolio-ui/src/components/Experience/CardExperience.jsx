6f12451b33aea492c9a3231fa056e203dc3ae3ba
moroniaguilera <moroniaguilera1993@gmail.com>
2026-03-25 22:16:34 +0000

    F #62 feat: update CardExperience component styles

--- portfolio-ui/src/components/Experience/CardExperience.jsx
+++ portfolio-ui/src/components/Experience/CardExperience.jsx
@@ -27,26 +27,26 @@ export default function CardExperience({
 }) {
   return (
     <>
-      <div className="flex flex-row gap-4 bg-(--brand-dark-light) p-4 rounded-2xl w-full tracking-[1.08px] shadow-(--brand-darker)">
-        <div className="w-[45%]">
+      <div className="flex flex-col md:flex-row gap-4 bg-(--brand-darker) p-4 rounded-2xl w-full tracking-[1.08px] shadow-(--brand-darker) justify-center">
+        <div className="sm:w-[45%]">
           {mode === "work" ? (
             <>
               <div className="flex flex-col gap-2">
-                <div className="flex flex-row gap-2 justify-between items-end text-(--brand-darker)">
-                  <span className="text-2xl">{data.company}</span>
+                <div className="flex flex-col md:flex-row gap-2 md:justify-between md:items-end text-white">
+                  <span className="text-xl lg:text-2xl">{data.company}</span>
                   <span className="font-normal ">{data.place}</span>
                 </div>
-                <div className="flex flex-row gap-2 justify-end items-end font-normal ">
+                <div className="flex flex-row gap-2 justify-end items-end font-normal text-(--brand-dark-light)">
                   <span>{data.position}</span>
                 </div>
-                <div className="font-normal text-[0.75rem] text-(--brand-dark)">
+                <div className="font-normal text-[0.75rem] text-(--brand-dark-light)">
                   <span>{data.description}</span>
                 </div>
-                <div className="flex flex-row gap-2">
+                <div className="flex flex-row gap-2 items-center">
                   {data.labels.map((label, index) => (
                     <span
                       key={index}
-                      className="text-[0.75rem] bg-white text-(--brand-dark) p-2 rounded-full font-bold"
+                      className="text-[0.6rem] lg:text-[0.75rem] bg-white text-(--brand-dark) p-2 rounded-full font-bold"
                     >
                       {label}
                     </span>
@@ -55,24 +55,24 @@ export default function CardExperience({
 
                 {data.links && data.links.length > 0 && (
                   <div className="flex flex-row gap-2 mt-2">
-                    {data.links.map((l, i) => (
+                    {data.links.map((link, index) => (
                       <a
-                        key={i}
-                        href={l.url}
+                        key={index}
+                        href={link.url}
                         target="_blank"
                         rel="noopener noreferrer"
-                        title={l.tooltip}
+                        title={link.tooltip}
                         className="inline-flex items-center justify-center p-1"
                       >
-                        {l.icon ? (
-                          l.icon.startsWith("http") ? (
+                        {link.icon ? (
+                          link.icon.startsWith("http") ? (
                             <img
-                              src={l.icon}
-                              alt={l.tooltip}
+                              src={link.icon}
+                              alt={link.tooltip}
                               className="w-4 h-4"
                             />
                           ) : (
-                            <i className={l.icon} />
+                            <i className={link.icon} />
                           )
                         ) : (
                           <span className="w-4 h-4 bg-gray-200 rounded-full" />
@@ -85,7 +85,7 @@ export default function CardExperience({
             </>
           ) : mode === "project" ? (
             <>
-              <div className="flex flex-col gap-2 justify-end items-end font-normal text-(--brand-darker)">
+              <div className="flex flex-col gap-2 justify-end items-end font-normal text-(--brand-dark-light)">
                 <span>{data.date}</span>
               </div>
             </>
@@ -93,7 +93,7 @@ export default function CardExperience({
             <></>
           )}
         </div>
-        <div className="flex flex-col items-center w-[10%]">
+        <div className="hidden md:flex flex-col items-center  sm:w-[10%] ">
           <div className="">
             <div className="w-5.5 h-5.5 bg-white rounded-full"></div>
           </div>
@@ -101,31 +101,31 @@ export default function CardExperience({
             <div className="border-l-4 border-white h-full"></div>
           </div>
         </div>
-        <div className="w-[45%]">
+        <div className="sm:w-[45%] ">
           {mode === "work" ? (
             <>
-              <div className="flex flex-col gap-2 justify-start items-start font-normal text-(--brand-darker)">
+              <div className="flex flex-col gap-2 justify-start items-start font-normal text-(--brand-dark-light)">
                 <span>{data.date}</span>
               </div>
             </>
           ) : mode === "project" ? (
             <>
               <div className="flex flex-col gap-2">
-                <div className="flex justify-start text-2xl text-(--accent-red) ">
+                <div className="flex justify-start text-xl lg:text-2xl text-(--accent-red) ">
                   <span>{data.company}</span>
                 </div>
                 <div className="flex justify-start font-normal">
                   <span>{data.type}</span>
                 </div>
-                <div className="font-normal text-[0.75rem] text-(--brand-dark)">
+                <div className="font-normal text-[0.75rem] text-(--brand-dark-light)">
                   <span>{data.description}</span>
                 </div>
                 <div className="flex flex-row gap-2 justify-between">
-                  <div className="flex flex-row gap-2">
+                  <div className="flex flex-row gap-2 items-center">
                     {data.labels.map((label, index) => (
                       <span
                         key={index}
-                        className="text-[0.75rem] bg-white text-(--brand-dark) p-2 rounded-full font-bold"
+                        className="text-[0.6rem] lg:text-[0.75rem] bg-white text-(--brand-dark) p-2 rounded-full font-bold"
                       >
                         {label}
                       </span>
