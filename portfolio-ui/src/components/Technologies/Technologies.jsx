2e63cd1c1266fbea703efb89a60a5d5b33b62e5a
moroniaguilera <moroniaguilera1993@gmail.com>
2026-03-29 23:01:26 +0000

    F #65 feat: enhance Technologies component with CardTechnologies and add translations for tech categories

--- portfolio-ui/src/components/Technologies/Technologies.jsx
+++ portfolio-ui/src/components/Technologies/Technologies.jsx
@@ -1,3 +1,65 @@
+import CardTechnologies from "./CardTechnologies";
+import javaicon from "../../assets/icons/java.svg";
+
 export default function Technologies() {
-  return <div>Technologies</div>;
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center gap-2 mt-4 ">
+        <div>
+          <span className="text-[1.5rem] md:text-[2rem] lg:text-[4rem]">
+            Technologies
+          </span>
+          <div>
+            <span className="text-[1.25rem] md:text-[1.5rem] lg:text-[3rem]">
+              Backend
+            </span>
+            <div className="flex flex-row gap-4">
+              <div>
+                <span>
+                  <CardTechnologies
+                    title="Java"
+                    icon={javaicon}
+                  ></CardTechnologies>
+                </span>
+              </div>
+              <div>
+                <span>2</span>
+              </div>
+            </div>
+          </div>
+          <div>
+            <span className="text-[1.25rem] md:text-[1.5rem] lg:text-[3rem]">
+              Frontend
+            </span>
+            <div className="flex flex-row">
+              <div>
+                <span>1</span>
+              </div>
+              <div>
+                <span>2</span>
+              </div>
+            </div>
+          </div>
+          <div>
+            <span className="text-[1.25rem] md:text-[1.5rem] lg:text-[3rem]">
+              DevOps
+            </span>
+            <div className="flex flex-row">
+              <div>
+                <span>1</span>
+              </div>
+              <div>
+                <span>2</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <span className="text-[1.5rem] md:text-[2rem] lg:text-[4rem] text-(--accent-red)">
+            More
+          </span>
+        </div>
+      </div>
+    </>
+  );
 }
